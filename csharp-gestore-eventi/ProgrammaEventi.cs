e0c2c7700e5e814081119936becb07c0a4ec7daa
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-18 11:15:52 +0200

    Inzio implementazione ProgrammaEventi

---
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class ProgrammaEventi
+    {
+        private string titolo;
+        private List<Evento> eventi;
+
+        public ProgrammaEventi(string titolo)
+        {
+            this.titolo = titolo;
+            eventi = new List<Evento>();
+        }
+    }
+}
